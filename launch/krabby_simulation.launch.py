feb90682082912b1b117bba94ab45ce36bbc9fa5
Victor on ROS2 VM <victor.dubois@free.fr>
2024-03-07 22:12:19 +0100

    start ROS2 conversion
    robot state publisher but no model spawn

---
+++ launch/krabby_simulation.launch.py
@@ -0,0 +1,119 @@
+# Author: Addison Sears-Collins
+# Date: September 19, 2021
+# Description: Load a world file into Gazebo.
+# https://automaticaddison.com
+ 
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration, PythonExpression
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+import xacro
+ 
+def generate_launch_description():
+ 
+  # Set the path to the Gazebo ROS package
+  pkg_gazebo_ros = FindPackageShare(package='gazebo_ros').find('gazebo_ros')   
+   
+  # Set the path to this package.
+  pkg_share = FindPackageShare(package='eurobot2020_gazebo').find('eurobot2020_gazebo')
+ 
+  # Set the path to the world file
+  world_file_name = 'eurobot2023_basic.world'
+  world_path = os.path.join(pkg_share, 'worlds', world_file_name)
+   
+  # Set the path to the SDF model files.
+  gazebo_models_path = os.path.join(pkg_share, 'models')
+  os.environ["GAZEBO_MODEL_PATH"] = gazebo_models_path
+
+  krabby_simulation_pkg = FindPackageShare(package='krabby_description').find('krabby_description')
+  krabby_simulation_urdf = os.path.join(krabby_simulation_pkg, 'urdf')
+  krabi_xacro_file_name =  os.path.join(krabby_simulation_urdf, 'krabby.xacro')
+  print("xacro_file_name : {}".format(krabi_xacro_file_name))
+
+
+  doc = xacro.process_file(krabi_xacro_file_name, mappings={})
+  robot_desc = doc.toprettyxml(indent='  ')
+  rsp_params = {'robot_description': robot_desc}
+  rsp = Node(package='robot_state_publisher',
+              executable='robot_state_publisher',
+              output='both',
+              parameters=[rsp_params])
+  
+  krabby_spawn_params = {'-entity': "krabby"}
+  krabby_spawn = Node(package='gazebo_ros',
+              executable='spawn_entity.py',
+              output='both',
+              parameters=[krabby_spawn_params])#"-urdf -param robot_description -entity krabby -x $(arg xRobotPos) -y $(arg yRobotPos) -z 1.0 -Y $(arg zRobotOrientation)"])
+  
+
+  ########### YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE ##############  
+  # Launch configuration variables specific to simulation
+  headless = LaunchConfiguration('headless')
+  use_sim_time = LaunchConfiguration('use_sim_time')
+  use_simulator = LaunchConfiguration('use_simulator')
+  world = LaunchConfiguration('world')
+ 
+  declare_simulator_cmd = DeclareLaunchArgument(
+    name='headless',
+    default_value='False',
+    description='Whether to execute gzclient')
+     
+  declare_use_sim_time_cmd = DeclareLaunchArgument(
+    name='use_sim_time',
+    default_value='true',
+    description='Use simulation (Gazebo) clock if true')
+ 
+  declare_use_simulator_cmd = DeclareLaunchArgument(
+    name='use_simulator',
+    default_value='True',
+    description='Whether to start the simulator')
+ 
+  declare_world_cmd = DeclareLaunchArgument(
+    name='world',
+    default_value=world_path,
+    description='Full path to the world model file to load')
+    
+  # Specify the actions
+   
+  # Start Gazebo server
+  start_gazebo_server_cmd = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')),
+    condition=IfCondition(use_simulator),
+    launch_arguments={'world': world}.items())
+ 
+  # Start Gazebo client    
+  start_gazebo_client_cmd = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')),
+    condition=IfCondition(PythonExpression([use_simulator, ' and not ', headless])))
+  
+  krabby_state_pub_node = Node(
+    package='robot_state_publisher',
+    executable='robot_state_publisher',
+    name='robot_state_publisher',
+    output='screen',
+    parameters=[{'use_sim_time': use_sim_time}],
+    arguments=[krabi_xacro_file_name]
+  )
+ 
+  # Create the launch description and populate
+  ld = LaunchDescription()#krabby_state_pub_node)
+
+
+  # Declare the launch options
+  ld.add_action(declare_simulator_cmd)
+  ld.add_action(declare_use_sim_time_cmd)
+  ld.add_action(declare_use_simulator_cmd)
+  ld.add_action(declare_world_cmd)
+  #ld.add_action(krabby_state_pub_node)
+  ld.add_action(rsp)
+  #ld.add_action(krabby_spawn)# @todo uncomment when it works
+ 
+  # Add any actions
+  ld.add_action(start_gazebo_server_cmd)
+  ld.add_action(start_gazebo_client_cmd)
+ 
+  return ld
