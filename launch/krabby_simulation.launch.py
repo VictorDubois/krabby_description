b484e04259fce53b13885f8577bd9e1697733913
Victor on ROS2 VM <victor.dubois@free.fr>
2024-05-01 21:38:48 +0200

    fix typos

--- launch/krabby_simulation.launch.py
+++ launch/krabby_simulation.launch.py
@@ -51,7 +51,7 @@ def generate_launch_description():
   
   isBlue_launch_arg = DeclareLaunchArgument(
     'isBlue',
-    default_value='false'
+    default_value='False'
   )
   xRobotPos_launch_arg = DeclareLaunchArgument(
     'xRobotPos',
@@ -95,7 +95,7 @@ def generate_launch_description():
      
   declare_use_sim_time_cmd = DeclareLaunchArgument(
     name='use_sim_time',
-    default_value='true',
+    default_value='True',
     description='Use simulation (Gazebo) clock if true')
  
   declare_use_simulator_cmd = DeclareLaunchArgument(
