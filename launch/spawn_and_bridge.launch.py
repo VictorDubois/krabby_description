a02cbc5f762ab0104fabf0177cce10ae027d7e9c
Victor Dubois <victor.dubois@navya.tech>
2025-04-04 19:01:10 +0200

    start working on initial position (does not match Krabi's world)

--- launch/spawn_and_bridge.launch.py
+++ launch/spawn_and_bridge.launch.py
@@ -7,6 +7,7 @@ import xacro
 from ament_index_python.packages import get_package_share_directory
 import os
 from launch_ros.actions import Node, PushRosNamespace
+from launch.substitutions import Command, LaunchConfiguration, PythonExpression
 
 
 def get_robot_description_string(pkg):
@@ -19,6 +20,28 @@ def get_robot_description_string(pkg):
 def generate_launch_description():
     pkg = get_package_share_directory("krabby_description")
 
+    isBlue_value = LaunchConfiguration('isBlue')
+    xRobotPos_value = LaunchConfiguration('xRobotPos')
+    yRobotPos_value = LaunchConfiguration('yRobotPos')
+    zRobotOrientation_value = LaunchConfiguration('zRobotOrientation')
+  
+    isBlue_launch_arg = DeclareLaunchArgument(
+        'isBlue',
+        default_value='False'
+    )
+    xRobotPos_launch_arg = DeclareLaunchArgument(
+        'xRobotPos',
+        default_value='1.25'
+    )
+    yRobotPos_launch_arg = DeclareLaunchArgument(
+        'yRobotPos',
+        default_value='0.5'
+    )
+    zRobotOrientation_launch_arg = DeclareLaunchArgument(
+        'zRobotOrientation',
+        default_value='0.0'
+    )
+
     robot_desc = get_robot_description_string(pkg)
     bridge_config_file = os.path.join(pkg, "params", "gz_bridge.yaml")
 
@@ -30,7 +53,10 @@ def generate_launch_description():
         launch_arguments=[
             ('bridge_name', 'gz_bridge'),
             ('model_string', robot_desc),
-            #('x', "0.5"), # can be used to define the full pose (xyzRPY)
+            ('x', xRobotPos_value),
+            ('y', yRobotPos_value),
+            #('z', "0.5"),
+            ("Y", zRobotOrientation_value),
             ('config_file', bridge_config_file),
         ])
 
